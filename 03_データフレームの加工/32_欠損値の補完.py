375055cc38066857f91fb3dee5c65410e2401e8b
Keido Sumi <delta0726@iuc-net.ne.jp>
2023-03-05 08:22:29 +0900

    Update 03-32

--- 03_データフレームの加工/32_欠損値の補完.py
+++ 03_データフレームの加工/32_欠損値の補完.py
@@ -1,17 +1,17 @@
-# ******************************************************************************
-# Project   : Pandasのメモ
-# Chapter   : 03 データフレームの加工
-# Title     : 32 欠損値の補完
-# Date      : 2023/02/25
-# ******************************************************************************
+"""
+Project   : Pandasのメモ
+Chapter   : 03 データフレームの加工
+Title     : 32 欠損値の補完
+Date      : 2023/02/25
+"""
 
 
 # ＜目次＞
 # 0 準備
-# 1 データフレーム全体を統一的に補完
-# 2 列単位で補完
-# 3 集計値で補完
-# 4 グループ単位の集計値で補完
+# 1 特定の値で補完
+# 2 集計値で補完
+# 3 グループ単位の集計値で補完
+# 4 直前値/直後値で補完
 
 
 # 0 準備 --------------------------------------------------------------------------
@@ -32,39 +32,45 @@ iris_lab = iris.filter(['species'])
 iris_nan = pd.concat([iris_num, iris_lab], axis=1)
 
 
-# 1 データフレーム全体を補完 ----------------------------------------------------------
+# 1 特定の値で補完 ------------------------------------------------------------------
 
-# フロント・フィル
-iris_nan.ffill()
+# 一律にゼロを指定
+iris_nan.fillna(value=0)
 
-# バック・フィル
-iris_nan.bfill()
-
-# 全ての欠損値を補完
-iris_nan.ffill().bfill()
+# 列ごとに数値を指定
+iris_nan.fillna(value={'sepal_length': 0, 'sepal_width': 1})
 
 
-# 2 列単位で補完 -----------------------------------------------------------------------
+# 2 集計値で補完 -----------------------------------------------------------------------
 
-# フロント・フィル
-iris_nan.assign(sepal_length=lambda x: x['sepal_length'].ffill())
+# 全ての列平均値で補完
+iris_nan.fillna(iris_nan.mean())
 
-# バック・フィル
-iris_nan.assign(sepal_length=lambda x: x['sepal_length'].bfill())
-
-# 全ての欠損値を補完
-iris_nan.assign(sepal_length=lambda x: x['sepal_length'].ffill().bfill())
-
-
-# 3 集計値で補完 -----------------------------------------------------------------------
-
-# データフレーム全体の平均値
+# 特定列を平均値で補完
 iris_nan.assign(sepal_length=lambda x: x.fillna(x['sepal_length'].mean()))
 
 
-# 4 グループ単位の集計値で補完 -------------------------------------------------------------
+# 3 グループ単位の集計値で補完 -------------------------------------------------------------
 
 # グループ単位の平均値
 iris_nan.assign(sepal_length=lambda x: x['sepal_length']
                 .fillna(x.groupby('species')['sepal_length'].transform('mean')))
 
+
+# 4 直前値/直後値で補完 ------------------------------------------------------------------
+
+# データフレーム全体の補完
+# --- フロント・フィル
+# --- バック・フィル
+# --- 全ての欠損値を補完
+iris_nan.ffill()
+iris_nan.bfill()
+iris_nan.ffill().bfill()
+
+# 列単位の補完
+# --- フロント・フィル
+# --- バック・フィル
+# --- 全ての欠損値を補完
+iris_nan.assign(sepal_length=lambda x: x['sepal_length'].ffill())
+iris_nan.assign(sepal_length=lambda x: x['sepal_length'].bfill())
+iris_nan.assign(sepal_length=lambda x: x['sepal_length'].ffill().bfill())
